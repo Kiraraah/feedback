07f87b881c59d5b1a68954c7f027d697bd36d735
Leislane Silva <Srtaleislane@gmail.com>
2024-06-11 00:25:15 +0000

    feat: instalação e implantação do react-router-dom e do style-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { ThemeProvider } from "styled-components"
+
+import {Home} from "./pages/Home"
+import { theme } from "./styles/theme"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>
+  }
+])
+
+
 export default function App() {
   return (
-    <div>App</div>
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+      <GlobalStyle/>
+    </ThemeProvider>
   )
 }
